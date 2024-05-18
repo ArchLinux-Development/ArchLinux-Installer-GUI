b4de747f576d5e68b63b92515092b4e04ec45a46
Eliminater74 <eliminater74@gmail.com>
2024-05-18 10:47:11 -0400

    fix bootloaders

--- frames/bootloader_setup_frame.py
+++ frames/bootloader_setup_frame.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
+from libs.bootloader import grub, systemd_boot, syslinux, refind, lilo, efistub, clover_efi, elilo
 
 class BootloaderSetupFrame(ttk.Frame):
     def __init__(self, parent):
@@ -7,11 +8,36 @@ class BootloaderSetupFrame(ttk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        ttk.Label(self, text="Bootloader Setup").pack(pady=5)
-        ttk.Label(self, text="Select Bootloader:").pack(pady=5)
+        ttk.Label(self, text="Bootloader Setup:").pack(pady=10)
 
-        self.bootloader_var = tk.StringVar()
-        self.bootloader_combo = ttk.Combobox(self, textvariable=self.bootloader_var)
-        self.bootloader_combo.pack(pady=5)
-        self.bootloader_combo['values'] = ["GRUB", "systemd-boot", "rEFInd"]
-        self.bootloader_combo.current(0)
+        self.bootloader_var = tk.StringVar(value="grub")
+        bootloaders = ["GRUB", "systemd-boot", "Syslinux", "rEFInd", "LILO", "EFISTUB", "Clover EFI", "Elilo"]
+
+        for bl in bootloaders:
+            ttk.Radiobutton(self, text=bl, variable=self.bootloader_var, value=bl.lower()).pack(pady=5)
+
+        self.install_button = ttk.Button(self, text="Install Bootloader", command=self.install_bootloader)
+        self.install_button.pack(pady=10)
+
+    def install_bootloader(self):
+        bootloader = self.bootloader_var.get()
+        if bootloader == "grub":
+            message = grub.install_grub()
+        elif bootloader == "systemd-boot":
+            message = systemd_boot.install_systemd_boot()
+        elif bootloader == "syslinux":
+            message = syslinux.install_syslinux()
+        elif bootloader == "refind":
+            message = refind.install_refind()
+        elif bootloader == "lilo":
+            message = lilo.install_lilo()
+        elif bootloader == "efistub":
+            message = efistub.install_efistub()
+        elif bootloader == "clover efi":
+            message = clover_efi.install_clover_efi()
+        elif bootloader == "elilo":
+            message = elilo.install_elilo()
+        else:
+            message = "Unknown bootloader selected"
+
+        messagebox.showinfo("Bootloader Installation", message)
