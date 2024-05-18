b6afe109e8e4ff691915112a031398e4c7e04b0c
Eliminater74 <eliminater74@gmail.com>
2024-05-18 12:46:17 -0400

    swap fix again

--- libs/swap.py
+++ libs/swap.py
@@ -22,17 +22,17 @@ class SwapSetup:
         self.zram_size_label.pack(side="left", padx=5)
 
         self.zram_size_var = tk.StringVar()
-        self.zram_size_combo = ttk.Combobox(self.zram_frame, textvariable=self.zram_size_var, width=10)
+        self.zram_size_combo = ttk.Combobox(self.zram_frame, textvariable=self.zram_size_var, width=20)
         self.zram_size_combo.pack(side="left", padx=5)
 
         self.populate_zram_sizes()
         self.zram_frame.pack(pady=5)
-        print("Initialized SwapSetup and populated ZRAM sizes")
 
     def populate_zram_sizes(self):
         total_ram_mb = psutil.virtual_memory().total // (1024 * 1024)  # Convert bytes to MB
-        zram_sizes_mb = [total_ram_mb // 4, total_ram_mb // 2, total_ram_mb]
-        zram_sizes_gb = [f"{size // 1024} GB" for size in zram_sizes_mb]
+        half_ram_mb = total_ram_mb // 2
+        zram_sizes_mb = [total_ram_mb // 4, half_ram_mb, total_ram_mb]
+        zram_sizes_gb = [f"{size // 1024} GB ({size} MB)" for size in zram_sizes_mb]
 
         self.zram_size_combo['values'] = zram_sizes_gb
         self.zram_size_var.set(zram_sizes_gb[1])  # Default to half of the total RAM
