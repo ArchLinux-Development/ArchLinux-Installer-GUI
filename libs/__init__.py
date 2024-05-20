abc98b8886a3c4a3262685b110b66ad912465f01
Eliminater74 <eliminater74@gmail.com>
2024-05-19 21:35:09 -0400

    fix up this

--- libs/__init__.py
+++ libs/__init__.py
@@ -7,3 +7,20 @@ from .bootloader.bootloader_setup import BootloaderSetup
 from .desktop_environment.desktop_environment_setup import DesktopEnvironmentSetup
 from .packages_setup import PackagesSetup
 from .kernel_setup import KernelSetup
+from .final_arch_installer import run_chaotic_aur_setup, run_cachyos_repo_setup
+
+__all__ = [
+    "UserInput",
+    "is_connected",
+    "scan_wifi",
+    "get_interfaces",
+    "SwapSetup",
+    "show_splash_screen",
+    "FilesystemSetup",
+    "BootloaderSetup",
+    "DesktopEnvironmentSetup",
+    "PackagesSetup",
+    "KernelSetup",
+    "run_chaotic_aur_setup",
+    "run_cachyos_repo_setup"
+]
